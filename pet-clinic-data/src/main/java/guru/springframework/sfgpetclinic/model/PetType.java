b1bc0cc6532c63b47fafdff41b882b3c6ff3444a
Djonathan Barros <djonathan.barros@gmail.com>
2019-01-22 20:40:35 -0200

    Added project Lombok to the project. Closes #51.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,20 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Data
 @Entity
 @Table(name = "pet_types")
 public class PetType extends BaseEntity {
 
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Data
+@EqualsAndHashCode(exclude = {"pet"})
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -15,27 +20,4 @@ public class Visit extends BaseEntity {
   @ManyToOne
   private Pet pet;
 
-  public LocalDate getDate() {
-    return date;
-  }
-
-  public void setDate(LocalDate date) {
-    this.date = date;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Pet getPet() {
-    return pet;
-  }
-
-  public void setPet(Pet pet) {
-    this.pet = pet;
-  }
 }
